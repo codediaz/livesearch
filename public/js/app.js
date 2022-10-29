56771eb8379a87cd676636cc4af9bcabc31be1be
Sergio Díaz <sdiazf99@outlook.com>
2022-10-29 15:56:40 -0500

    feature: skeleton structure

--- public/js/app.js
+++ public/js/app.js
@@ -19487,9 +19487,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({
   data: function data() {
-    return {
-      text: "Hello World"
-    };
+    return {};
   }
 });
 
@@ -36743,12 +36741,12 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-/* harmony import */ var _App_vue_vue_type_template_id_6e43a170_lang_true_bindings_text_data___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./App.vue?vue&type=template&id=6e43a170&lang=true&bindings={"text":"data"} */ "./resources/vue/App.vue?vue&type=template&id=6e43a170&lang=true&bindings={\"text\":\"data\"}");
+/* harmony import */ var _App_vue_vue_type_template_id_6e43a170_lang_true_bindings___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./App.vue?vue&type=template&id=6e43a170&lang=true&bindings={} */ "./resources/vue/App.vue?vue&type=template&id=6e43a170&lang=true&bindings={}");
 /* harmony import */ var _App_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./App.vue?vue&type=script&lang=js */ "./resources/vue/App.vue?vue&type=script&lang=js");
 
 
 
-_App_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"].render = _App_vue_vue_type_template_id_6e43a170_lang_true_bindings_text_data___WEBPACK_IMPORTED_MODULE_0__.render
+_App_vue_vue_type_script_lang_js__WEBPACK_IMPORTED_MODULE_1__["default"].render = _App_vue_vue_type_template_id_6e43a170_lang_true_bindings___WEBPACK_IMPORTED_MODULE_0__.render
 /* hot reload */
 if (false) {}
 
@@ -36774,26 +36772,26 @@ __webpack_require__.r(__webpack_exports__);
 
 /***/ }),
 
-/***/ "./resources/vue/App.vue?vue&type=template&id=6e43a170&lang=true&bindings={\"text\":\"data\"}":
-/*!************************************************************************************************!*\
-  !*** ./resources/vue/App.vue?vue&type=template&id=6e43a170&lang=true&bindings={"text":"data"} ***!
-  \************************************************************************************************/
+/***/ "./resources/vue/App.vue?vue&type=template&id=6e43a170&lang=true&bindings={}":
+/*!***********************************************************************************!*\
+  !*** ./resources/vue/App.vue?vue&type=template&id=6e43a170&lang=true&bindings={} ***!
+  \***********************************************************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
-/* harmony export */   "render": () => (/* reexport safe */ _node_modules_vue_loader_dist_templateLoader_js_ruleSet_1_rules_2_node_modules_vue_loader_dist_index_js_ruleSet_0_use_0_App_vue_vue_type_template_id_6e43a170_lang_true_bindings_text_data___WEBPACK_IMPORTED_MODULE_0__.render)
+/* harmony export */   "render": () => (/* reexport safe */ _node_modules_vue_loader_dist_templateLoader_js_ruleSet_1_rules_2_node_modules_vue_loader_dist_index_js_ruleSet_0_use_0_App_vue_vue_type_template_id_6e43a170_lang_true_bindings___WEBPACK_IMPORTED_MODULE_0__.render)
 /* harmony export */ });
-/* harmony import */ var _node_modules_vue_loader_dist_templateLoader_js_ruleSet_1_rules_2_node_modules_vue_loader_dist_index_js_ruleSet_0_use_0_App_vue_vue_type_template_id_6e43a170_lang_true_bindings_text_data___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[2]!../../node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./App.vue?vue&type=template&id=6e43a170&lang=true&bindings={"text":"data"} */ "./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[2]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./resources/vue/App.vue?vue&type=template&id=6e43a170&lang=true&bindings={\"text\":\"data\"}");
+/* harmony import */ var _node_modules_vue_loader_dist_templateLoader_js_ruleSet_1_rules_2_node_modules_vue_loader_dist_index_js_ruleSet_0_use_0_App_vue_vue_type_template_id_6e43a170_lang_true_bindings___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[2]!../../node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./App.vue?vue&type=template&id=6e43a170&lang=true&bindings={} */ "./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[2]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./resources/vue/App.vue?vue&type=template&id=6e43a170&lang=true&bindings={}");
 
 
 /***/ }),
 
-/***/ "./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[2]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./resources/vue/App.vue?vue&type=template&id=6e43a170&lang=true&bindings={\"text\":\"data\"}":
-/*!*********************************************************************************************************************************************************************************************************************************!*\
-  !*** ./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[2]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./resources/vue/App.vue?vue&type=template&id=6e43a170&lang=true&bindings={"text":"data"} ***!
-  \*********************************************************************************************************************************************************************************************************************************/
+/***/ "./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[2]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./resources/vue/App.vue?vue&type=template&id=6e43a170&lang=true&bindings={}":
+/*!********************************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/dist/templateLoader.js??ruleSet[1].rules[2]!./node_modules/vue-loader/dist/index.js??ruleSet[0].use[0]!./resources/vue/App.vue?vue&type=template&id=6e43a170&lang=true&bindings={} ***!
+  \********************************************************************************************************************************************************************************************************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
@@ -36805,10 +36803,43 @@ __webpack_require__.r(__webpack_exports__);
 
 
 const _hoisted_1 = { id: "appWrapper" }
+const _hoisted_2 = { class: "contanier" }
+const _hoisted_3 = { class: "search-box" }
+const _hoisted_4 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("input", {
+  type: "text",
+  class: "search-input",
+  name: "q",
+  placeholder: "Escribe una palabra..."
+}, null, -1 /* HOISTED */)
+const _hoisted_5 = { class: "result-list-show" }
+const _hoisted_6 = { class: "result-item" }
+const _hoisted_7 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("a", {
+  href: "#",
+  class: "result-link"
+}, [
+  /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", { class: "result-tittle" }, "Nombre del post"),
+  /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", { class: "result-content" }, " Lorem ipsum dolor sit amet consectetur adipisicing elit. ")
+], -1 /* HOISTED */)
+const _hoisted_8 = [
+  _hoisted_7
+]
+const _hoisted_9 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", null, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique nobis architecto dolorem non maiores perspiciatis eligendi, explicabo assumenda ullam sapiente soluta, impedit ipsa quaerat unde asperiores fugit velit cumque atque. Ab magni voluptatibus est eius provident atque, facere recusandae! Commodi eos vitae ex quisquam quidem eligendi tempora, quam, veritatis tempore nulla ratione sit itaque reprehenderit suscipit, voluptates pariatur voluptatem quod!", -1 /* HOISTED */)
+const _hoisted_10 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", null, "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique nobis architecto dolorem non maiores perspiciatis eligendi, explicabo assumenda ullam sapiente soluta, impedit ipsa quaerat unde asperiores fugit velit cumque atque. Ab magni voluptatibus est eius provident atque, facere recusandae! Commodi eos vitae ex quisquam quidem eligendi tempora, quam, veritatis tempore nulla ratione sit itaque reprehenderit suscipit, voluptates pariatur voluptatem quod!", -1 /* HOISTED */)
 
 function render(_ctx, _cache, $props, $setup, $data, $options) {
   return ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_1, [
-    (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($data.text), 1 /* TEXT */)
+    (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_2, [
+      (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_3, [
+        _hoisted_4,
+        (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("ul", _hoisted_5, [
+          ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, (0,vue__WEBPACK_IMPORTED_MODULE_0__.renderList)(10, (x) => {
+            return (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("li", _hoisted_6, _hoisted_8)
+          }), 64 /* STABLE_FRAGMENT */))
+        ])
+      ]),
+      _hoisted_9,
+      _hoisted_10
+    ])
   ]))
 }
 
--- resources/vue/App.vue
+++ resources/vue/App.vue
@@ -1,6 +1,25 @@
 <template lang="">
     <div id="appWrapper">
-        <p>{{ text }}</p>
+       <div class="contanier">
+        <div class="search-box">
+            <input type="text" class="search-input" name="q" placeholder="Escribe una palabra...">
+            <ul class="result-list-show">
+                <li
+                v-for="x in 10" class="result-item">
+                    <a href="#" class="result-link">
+                        <div class="result-tittle">Nombre del post</div>
+                        <div class="result-content">
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique nobis architecto dolorem non maiores perspiciatis eligendi, explicabo assumenda ullam sapiente soluta, impedit ipsa quaerat unde asperiores fugit velit cumque atque.
+        Ab magni voluptatibus est eius provident atque, facere recusandae! Commodi eos vitae ex quisquam quidem eligendi tempora, quam, veritatis tempore nulla ratione sit itaque reprehenderit suscipit, voluptates pariatur voluptatem quod!</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique nobis architecto dolorem non maiores perspiciatis eligendi, explicabo assumenda ullam sapiente soluta, impedit ipsa quaerat unde asperiores fugit velit cumque atque.
+        Ab magni voluptatibus est eius provident atque, facere recusandae! Commodi eos vitae ex quisquam quidem eligendi tempora, quam, veritatis tempore nulla ratione sit itaque reprehenderit suscipit, voluptates pariatur voluptatem quod!</p>
+       </div>
     </div>
 </template>
 <script>
@@ -8,7 +27,7 @@ export default {
 
     data(){
         return{
-            text: "Hello World"
+            
         }
     }
 
