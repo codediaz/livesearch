c5595aff61d99dd59f02849849446e1471d9924c
Sergio Díaz <sdiazf99@outlook.com>
2022-10-29 15:41:25 -0500

    feature: init WorkSpace - Vuejs, laravel-mix, webpack added

---
+++ resources/js/bootstrap.js
@@ -0,0 +1,14 @@
+//LOADASH
+window._ = require('lodash');
+
+//AXIOS
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+//VUE   
+import {createApp} from 'vue'
+import App from '../vue/App.vue'
+
+window.app = createApp(App);
+window.vm = app.mount('#app')
+
---
+++ resources/vue/App.vue
@@ -0,0 +1,20 @@
+<template lang="">
+    <div id="appWrapper">
+        <p>{{ text }}</p>
+    </div>
+</template>
+<script>
+export default {
+
+    data(){
+        return{
+            text: "Hello World"
+        }
+    }
+
+    
+}
+</script>
+<style lang="">
+    
+</style>
